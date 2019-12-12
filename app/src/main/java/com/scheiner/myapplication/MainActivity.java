0f64c44b819b410c9e0801ad59f8c9e39a1714ae
sscheiner <sf.scheiner@gmail.com>
2019-12-11 19:28:12 -0600

    added button to main activity and example onclick handler

--- app/src/main/java/com/scheiner/myapplication/MainActivity.java
+++ app/src/main/java/com/scheiner/myapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package com.scheiner.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Date;
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    //handle button click
+    public void startStopTimer(View view){
+
+        //handle main timer button
+        Button btn = findViewById(R.id.timerButton);
+        //get current time
+        Date date = new Date();
+        long currentMillis = date.getTime();
+
+        //print milliseconds
+        TextView clock = findViewById(R.id.millis);
+        clock.setText("hello world");
+
+    }
+
+
+
+
+
+
 }
